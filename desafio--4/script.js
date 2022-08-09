a34ce3fa4d8cf7733c2c2ea00bbc388cbc40823d
John Perez <tatozan@gmail.com>
2022-08-09 16:43:28 -0300

    desafio--4 v3

--- desafio--4/script.js
+++ desafio--4/script.js
@@ -43,7 +43,7 @@ class Operacion{
     constructor(numeroOperacion = 0, tipoOperacion, par = "predeterminado", distanciaPorcentajeRecompraReventa = 0, aumentoPorcentajeRecompraReventa = 0, sl, precioMoneda = 0, cantidadMonedas = 0, montoInvertido = 0){
         this.numeroOperacion = numeroOperacion;
         this.tipoOperacion = tipoOperacion;
-        this.par = par.toUpperCase();
+        this.par = par
         this.distanciaPorcentajeRecompraReventa = distanciaPorcentajeRecompraReventa;
         this.aumentoPorcentajeRecompraReventa = aumentoPorcentajeRecompraReventa;
         this.sl = sl;
@@ -57,7 +57,7 @@ class Operacion{
         this.montoInvertido = precioMoneda * cantidadMonedas;
     }
     
-    mostrarDatosOperacion(){
+    mostrarDatosOperacionInicial(){
         const divDatosCompraInicial = document.getElementById('divDatosCompraInicial');
 
         divDatosCompraInicial.innerHTML += `
@@ -65,35 +65,67 @@ class Operacion{
         <p> Precio de compra: $${this.precioMoneda} </p>
         <p> Monto invertido en dolares: $${this.montoInvertido.toFixed(3)} </p>
         <p> Tamaño compra: ${this.cantidadMonedas} monedas </p>
-        <p> Tipo de operacion: ${this.tipoOperacion} </p>
-        
-        `
+        <p> Tipo de operacion: ${this.tipoOperacion} </p>    `
     }
 
 }
 
-let tipoOperacion, par, distanciaPorcentajeRecompraReventa, aumentoPorcentajeRecompraReventa, sl, precioMoneda, cantidadMonedas;
-let montoInvertido;
-//let numeroRecomprasTotales;
-//let mostrarListaInvertida;
+function mostrarOperaciones(operaciones, gridOperaciones){
+    gridOperaciones.innerHTML += `
+  
+    <div class="row">
+        <div class="col"> # </div>
+        <div class="col"> PRECIO </div>
+        <div class="col"> MONEDA </div>
+        <div class="col"> USDT </div>
+    </div>
+    `
+    console.log(operaciones);
+    operaciones.forEach(operacion => {
+        console.log(`precio moneda operacion: ${operacion.precioMoneda}`);
+        gridOperaciones.innerHTML += `  
+        <div class="row">
+        <div class="col"> ${operacion.numeroOperacion} </div>
+        <div class="col"> $${operacion.precioMoneda.toFixed(3)} </div>
+        <div class="col"> ${operacion.cantidadMonedas.toFixed(3)} </div>
+        <div class="col"> $${operacion.montoInvertido.toFixed(2)} </div>
+        </div>
+        `
+    });
+}
 
-//contiene las operaciones, de recompra o reventa, incluida compra/venta incial
-const operaciones = [];
+function calcularPnL(cantidadMonedas, precioMonedaInicial, precioMonedaFinal, tipoOperacion){
+    let pnl;
+
+    if(tipoOperacion == "short"){
+        pnl = cantidadMonedas * (precioMonedaFinal - precioMonedaInicial);
+    } else if(tipoOperacion == "long"){
+        pnl = (cantidadMonedas * (precioMonedaFinal - precioMonedaInicial)) * -1;
+    }
+    
+    return pnl;
+        //pnl = operaciones[0].cantidadMonedas * (operaciones[nroOperacionAnterior].precioMoneda - precioMoneda);
+        
+}
 
 //compruebo si esta creado mi localStorage, si no esta creado lo creo, y si esta, le envio las operaciones que tenia previamente
 const operacionesIniciales = JSON.parse(localStorage.getItem("operacionesIniciales")) ?? [];
-
-//contiene todas las operaciones promediadas
-const operacionesProm = [];
+//console.log(operacionesIniciales);
 
 const form = document.getElementById('idForm');
+const idBtnDropDown = document.getElementById("idBtnDropDown");
+const idOperacionesInicialesLista = document.getElementById("idOperacionesInicialesLista");
+const gridOperaciones = document.getElementById("gridOperaciones");
 
-const idOperacionesIniciales = document.getElementById("idOperacionesIniciales");
+idBtnDropDown.addEventListener("click", () => {
 
-idOperacionesIniciales.addEventListener("click", () => {
+    //elimino todos los hijos de la lista, para cuando lo clickee no se repitan
+    idOperacionesInicialesLista.innerHTML = "";
+    console.log(`Operaciones iniciales lista: ${idOperacionesInicialesLista}`);
+
     operacionesIniciales.forEach(operacion => {
-        idOperacionesIniciales.innerHTML += `
-        <li><a class="${operacion}">${operacion.par}</a></li>  
+        idOperacionesInicialesLista.innerHTML += `
+        <li><p>${operacion} - ${operacion.par}</p></li>  
         `
     });
 });
@@ -101,20 +133,53 @@ idOperacionesIniciales.addEventListener("click", () => {
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
+    let tipoOperacion, par, distanciaPorcentajeRecompraReventa, aumentoPorcentajeRecompraReventa, sl, precioMoneda, cantidadMonedas;
+    let montoInvertido;
+    //let numeroRecomprasTotales;
+    //let mostrarListaInvertida;
+
+    //compruebo si mi localStorage hay almacenadas operaciones, sino creo una nueva
+    //la coloco aqui tambien porque si
+    const operacionesIniciales = JSON.parse(localStorage.getItem("operacionesIniciales")) ?? [];
+
+    //contiene las operaciones, de recompra o reventa, incluida compra/venta incial
+    const operaciones = [];
+    //contiene todas las operaciones promediadas
+    const operacionesProm = [];
+
+    const divDatosCompraInicial = document.getElementById("divDatosCompraInicial");
+    divDatosCompraInicial.innerHTML ="";
+
+    const gridOperaciones = document.getElementById("gridOperaciones");
+    gridOperaciones.innerHTML = "";
+
+    //metodo de js que extrae la informacion de los campos de un formulario objetivo
+    let dataForm = new FormData(event.target);
+
+    //hago este paso para que sea mas legible pero podria directamente crear el objeto con los datos que vengan del objeto dataForm
+    tipoOperacion = dataForm.get("operacion");
+    par = dataForm.get("parName");
+    distanciaPorcentajeRecompraReventa = parseFloat(dataForm.get("distanciaPorcentajeRecompraReventaName"));
+    aumentoPorcentajeRecompraReventa = parseFloat(dataForm.get("aumentoPorcentajeRecompraReventaName"));
+    sl = parseFloat(dataForm.get("slName"));
+    precioMoneda = parseFloat(dataForm.get("precioMonedaName"));
+    cantidadMonedas = parseFloat(dataForm.get("cantidadMonedasName"));
+
     //almaceno el valor de los input del html en variables let
-    tipoOperacion = (document.getElementById('tipoOperacion').value);
-    par = (document.getElementById('par').value);
+    /*tipoOperacion = (document.getElementById('tipoOperacion').value);
+    par = (document.getElementById('par').value).toUpperCase();
     distanciaPorcentajeRecompraReventa = parseFloat(document.getElementById('distanciaPorcentajeRecompraReventa').value);
     aumentoPorcentajeRecompraReventa = parseFloat(document.getElementById('aumentoPorcentajeRecompraReventa').value);
     sl = parseFloat(document.getElementById('sl').value);
     precioMoneda = parseFloat(document.getElementById('precioMoneda').value);
-    cantidadMonedas = parseFloat(document.getElementById('cantidadMonedas').value);
+
+    cantidadMonedas = parseFloat(document.getElementById('cantidadMonedas').value);*/
 
     //creo el objeto inicial, con los datos que me ingresaron en el html
     const operacion0 = new Operacion(0, tipoOperacion, par, distanciaPorcentajeRecompraReventa, aumentoPorcentajeRecompraReventa, sl, precioMoneda, cantidadMonedas);
     operaciones.push(operacion0);
 
-    operaciones[0].montoInvertido = precioMoneda * cantidadMonedas;
+    operaciones[0].calcularMontoInvertido(precioMoneda, cantidadMonedas);
 
     //operaciones[0].calcularRecomprasReventas(sl, distanciaPorcentajeRecompraReventa);
 
@@ -131,7 +196,7 @@ form.addEventListener('submit', (event) => {
 
     if(operaciones[0].tipoOperacion === "short"){
 
-        operaciones[0].mostrarDatosOperacion();
+        operaciones[0].mostrarDatosOperacionInicial();
 
         do{
             i += 1; 
@@ -151,18 +216,19 @@ form.addEventListener('submit', (event) => {
             let inversionProm;
             
             if(i === 1){
-                pnl = operaciones[0].cantidadMonedas * (precioMoneda - operaciones[nroOperacionAnterior].precioMoneda);
+                pnl = calcularPnL(operaciones[0].cantidadMonedas, operaciones[nroOperacionAnterior].precioMoneda, precioMoneda, operaciones[0].tipoOperacion);
 
             } else {
                 nroOperacionProm = nroOperacionAnterior - 1;
-                pnl = operacionesProm[nroOperacionProm].cantidadMonedas * (precioMoneda - operacionesProm[nroOperacionProm].precioMoneda);
-            
+
+                pnl = calcularPnL(operacionesProm[nroOperacionProm].cantidadMonedas, operacionesProm[nroOperacionProm].precioMoneda, precioMoneda, operaciones[0].tipoOperacion);
+                //pnl = operacionesProm[nroOperacionProm].cantidadMonedas * (precioMoneda - operacionesProm[nroOperacionProm].precioMoneda);
+
             }
 
             if(pnl <= operaciones[0].sl){
                 const operacion = new Operacion(nroOperacion, "short", par, operaciones[0].distanciaPorcentajeRecompraReventa, operaciones[0].aumentoPorcentajeRecompraReventa, operaciones[0].sl, precioMoneda, cantidadMonedas, inversion);    
                 operaciones.push(operacion);
-
 
                 if(i === 1){
                     precioMonedaProm = (operaciones[nroOperacionAnterior].montoInvertido + operaciones[nroOperacion].montoInvertido) / (operaciones[nroOperacionAnterior].cantidadMonedas + operaciones[nroOperacion].cantidadMonedas);
@@ -181,15 +247,15 @@ form.addEventListener('submit', (event) => {
             
             
         } while(pnl <= operaciones[0].sl && i < 8);
-
+            
         //guardo datos de mis operaciones para localStorage
         operacionesIniciales.push(operacion0);
 
         localStorage.setItem("operacionesIniciales", JSON.stringify(operacionesIniciales));
 
-        const gridOperaciones = document.getElementById('gridOperaciones');
+        mostrarOperaciones(operaciones, gridOperaciones);
 
-        gridOperaciones.innerHTML += `
+        /*gridOperaciones.innerHTML += `
   
         <div class="row">
             <div class="col"> # </div>
@@ -209,7 +275,8 @@ form.addEventListener('submit', (event) => {
             </div>
             `
         });
-        
+        */
+
         let operacionPromUltima = operacionesProm.length - 1;
         //formula calculo precio de moneda cuando toca SL que elegi como dato de entrada en USDT.
         precioMonedaEnSl = operacionesProm[operacionPromUltima].precioMoneda + (operaciones[0].sl / operacionesProm[operacionPromUltima].cantidadMonedas);
@@ -224,7 +291,7 @@ form.addEventListener('submit', (event) => {
         Monto total invertido utilizando todas las recompras: $${operacionesProm[operacionPromUltima].montoInvertido.toFixed(3)}`);
 
     } else if(tipoOperacion === "long"){
-        operaciones[0].mostrarDatosOperacion();
+        operaciones[0].mostrarDatosOperacionInicial();
 
         do{
             i += 1; 
@@ -244,11 +311,13 @@ form.addEventListener('submit', (event) => {
             let inversionProm;
             
             if(i === 1){
-                pnl = operaciones[0].cantidadMonedas * (operaciones[nroOperacionAnterior].precioMoneda - precioMoneda);
+                pnl = calcularPnL(operaciones[0].cantidadMonedas, operaciones[nroOperacionAnterior].precioMoneda, precioMoneda, operaciones[0].tipoOperacion);
+                //pnl = operaciones[0].cantidadMonedas * (operaciones[nroOperacionAnterior].precioMoneda - precioMoneda);
 
             } else {
                 nroOperacionProm = nroOperacionAnterior - 1;
-                pnl = operacionesProm[nroOperacionProm].cantidadMonedas * (operacionesProm[nroOperacionProm].precioMoneda - precioMoneda);
+                pnl = calcularPnL(operacionesProm[nroOperacionProm].cantidadMonedas, operacionesProm[nroOperacionProm].precioMoneda, precioMoneda, operaciones[0].tipoOperacion);
+                //pnl = operacionesProm[nroOperacionProm].cantidadMonedas * (operacionesProm[nroOperacionProm].precioMoneda - precioMoneda);
             
             }
 
@@ -315,6 +384,9 @@ form.addEventListener('submit', (event) => {
         Monto total invertido utilizando todas las recompras: $${operacionesProm[operacionPromUltima].montoInvertido.toFixed(3)}`);
 
     }
+
+    //reseteo el formulario
+    form.reset();
 });
 
 
